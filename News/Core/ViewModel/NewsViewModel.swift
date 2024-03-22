2e5bb08bf981a6b58ccb11c85a12a6c1298825e4
Benji Loya <benjiloya.apple@gmail.com>
2024-03-22 19:54:43 +0500

    Create Goup News

---
+++ News/Core/ViewModel/NewsViewModel.swift
@@ -0,0 +1,18 @@
+//
+//  NewsViewModel.swift
+//  News
+//
+//  Created by Benji Loya on 22.03.2024.
+//
+
+import SwiftUI
+
+struct NewsViewModel: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NewsViewModel()
+}
