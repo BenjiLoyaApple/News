859692b03153108c1d32efe39a7beb758b611ef2
Benji Loya <benjiloya.apple@gmail.com>
2024-03-22 19:36:56 +0500

    Initial Commit

---
+++ News/NewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsApp.swift
+//  News
+//
+//  Created by Benji Loya on 22.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct NewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
