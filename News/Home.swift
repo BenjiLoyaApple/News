f41e4e28e44a6aea973de16c8521db9f8443df8a
Benji Loya <benjiloya.apple@gmail.com>
2024-03-22 19:38:02 +0500

    123

---
+++ News/Home.swift
@@ -0,0 +1,18 @@
+//
+//  Home.swift
+//  News
+//
+//  Created by Benji Loya on 22.03.2024.
+//
+
+import SwiftUI
+
+struct Home: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Home()
+}
